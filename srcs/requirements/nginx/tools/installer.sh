aceaf3c6c744fd5879d6bc2b405964a1245ae98c
Mohammed Handa <mohammedhanda109@gmail.com>
2023-06-16 22:20:28 +0100

    nginx_installed_and_working

--- srcs/requirements/nginx/tools/installer.sh
+++ srcs/requirements/nginx/tools/installer.sh
@@ -7,5 +7,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
  -keyout /etc/ssl/private/CAR.key \
  -out /etc/ssl/certs/CAR.crt \
  -subj="/CN=mhanda/O=mhanda.1337.ma/C=US/L=FLORIDA"
+
 echo "<h1>hello world</h1>" > /var/www/html/index.html
+
 nginx -g "daemon off;"
