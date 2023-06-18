a83748fa7cbd88085335d272d2026c4a25b8e067
Mohammed Handa <mohammedhanda109@gmail.com>
2023-06-18 16:44:25 +0100

    some_updates

--- srcs/requirements/wordpress/tools/installer.sh
+++ srcs/requirements/wordpress/tools/installer.sh
@@ -12,6 +12,6 @@ wp core download --allow-root --locale=en_US
 wp config create --dbname=${DATABASE_NAME} --dbuser=${MYSQL_USER} --dbhost=${DATABASE_NAME} --dbpass=${MYSQL_PASSWORD} --allow-root
 wp core install --url=${DOMAIN} --title=${DOMAIN} --admin_user=${MYSQL_USER} --admin_password=${MYSQL_PASSWORD} --admin_email=${ADMIN_EMAIL} --allow-root
 wp user create ${ADMIN_USER} ${ADMIN_EMAIL_TWO} --role=administrator --user_pass=${MYSQL_PASSWORD} --allow-root
-mkdir /run/php && chown root:root /run/php && chmod 777 /run/php
+mkdir -p /run/php && chown root:root /run/php && chmod 777 /run/php
 sed -i 's#/run/php/php7.3-fpm.sock#9000#g' /etc/php/7.3/fpm/pool.d/www.conf
 php-fpm7.3 --nodaemonize
