f784f5c9169c6c5271e496f05488061de5d6b7ad
Mohammed Handa <mohammedhanda109@gmail.com>
2023-06-18 03:00:38 +0100

    wordpress_almost_done

--- srcs/requirements/wordpress/tools/installer.sh
+++ srcs/requirements/wordpress/tools/installer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-wget https://wordpress.org/latest.zip
-apt install unzip -y
-
-mkdir -p /var/www/html
-chwon -R www-data:www-data /var/www/html/
-unzip latest.zip -d /var/www/html
-rm -rf latest.zip
-mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+apt install -y curl
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod 777 wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+cd ${LOCAL}
+chown -R www-data:www-data ${LOCAL}
+wp core download --allow-root
+wp config create --dbname=${DATABASE_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --locale=${LOCAL} --allow-root
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 tail -f /dev/null
